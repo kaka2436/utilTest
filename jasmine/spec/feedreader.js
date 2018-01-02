c3085762d978cf5ff39fe0fe8d939b7c136da6a6
kaka2436 <kaka2436@163.com>
2018-01-02 22:28:35 +0800

    完成功能，缺少README文档

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -6,60 +6,85 @@
 /* 我们把所有的测试都放在了 $() 函数里面。因为有些测试需要 DOM 元素。
  * 我们得保证在 DOM 准备好之前他们不会被运行。
  */
+/*
+ * 设置超时时间为20秒，防止在网速差的情况下，导致测试条目不通过
+ */
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
 $(function() {
-    /* 这是我们第一个测试用例 - 其中包含了一定数量的测试。这个用例的测试
-     * 都是关于 Rss 源的定义的，也就是应用中的 allFeeds 变量。
-    */
     describe('RSS Feeds', function() {
-        /* 这是我们的第一个测试 - 它用来保证 allFeeds 变量被定义了而且
-         * 不是空的。在你开始做这个项目剩下的工作之前最好实验一下这个测试
-         * 比如你把 app.js 里面的 allFeeds 变量变成一个空的数组然后刷新
-         * 页面看看会发生什么。
-        */
+        //测试allFeeds被定义了，并且内容不为空;
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
+        //测试 allFeeds 对象里面的所有的源来保证有链接字段而且链接不是空的。
+        it('allFeeds 对象里面的所有的源来保证有链接字段而且链接不是空的',function () {
+            for(var i = 0 ; i < allFeeds.length;i++){
+                expect(allFeeds[i].url).toBeDefined();
+                expect(allFeeds[i].url.length).not.toBe(0);
+            }
+        });
 
+        //测试 allFeeds 对象里面的所有的源来保证有名字字段而且不是空的。
+        it('allFeeds 对象里面的所有的源来保证有名字字段而且不是空的',function () {
+            for (var i = 0 ; i < allFeeds.length;i++){
+                expect(allFeeds[i].name).toBeDefined();
+                expect(allFeeds[i].name.length).not.toBe(0);
+            }
+        });
+    });
 
-        /* TODO:
-         * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有链接字段而且链接不是空的。
-         */
-
-
-        /* TODO:
-         * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有名字字段而且不是空的。
-         */
+    /* 测试菜单元素默认是隐藏的。
+     * 当点击图标的时候菜单是否显示，
+     * 再次点击的时候是否隐藏。
+     */
+    describe('The menu',function () {
+        var $body;
+        beforeEach(function () {
+            $body = $('body');
+        });
+        it('保证菜单元素默认是隐藏的',function () {
+            expect($body.hasClass('menu-hidden')).toBe(true);
+        });
+        it('保证当菜单图标被点击的时候菜单会且切换可见状态',function () {
+            var $menuIcon = $('.menu-icon-link');
+            $menuIcon.click();
+            expect($body.hasClass('menu-hidden')).toBe(false);
+            $menuIcon.click();
+            expect($body.hasClass('menu-hidden')).toBe(true);
+        });
     });
 
 
-    /* TODO: 写一个叫做 "The menu" 的测试用例 */
+    /* 测试loadFeed 函数被调用而且工作正常，即在 .feed 容器元素
+     * 里面至少有一个 .entry 的元素。
+     */
+    describe('Initial Entries',function () {
+        var spyObj;
+        beforeEach(function (done) {
+            loadFeed(0,done);
+        });
 
-        /* TODO:
-         * 写一个测试用例保证菜单元素默认是隐藏的。你需要分析 html 和 css
-         * 来搞清楚我们是怎么实现隐藏/展示菜单元素的。
-         */
+        it('保证 loadFeed 函数被调用而且工作正常',function (done) {
+            expect($('.feed').find('.entry').length).toBeGreaterThan(0);
+            done();
+        });
+    });
 
-         /* TODO:
-          * 写一个测试用例保证当菜单图标被点击的时候菜单会切换可见状态。这个
-          * 测试应该包含两个 expectation ： 党点击图标的时候菜单是否显示，
-          * 再次点击的时候是否隐藏。
-          */
-
-    /* TODO: 13. 写一个叫做 "Initial Entries" 的测试用例 */
-
-        /* TODO:
-         * 写一个测试保证 loadFeed 函数被调用而且工作正常，即在 .feed 容器元素
-         * 里面至少有一个 .entry 的元素。
-         *
-         * 记住 loadFeed() 函数是异步的所以这个而是应该使用 Jasmine 的 beforeEach
-         * 和异步的 done() 函数。
-         */
-
-    /* TODO: 写一个叫做 "New Feed Selection" 的测试用例 */
-
-        /* TODO:
-         * 写一个测试保证当用 loadFeed 函数加载一个新源的时候内容会真的改变。
-         * 记住，loadFeed() 函数是异步的。
-         */
+    /* 测试当用 loadFeed 函数加载一个新源的时候内容会真的改变。
+     * 做法是通过对比 loadFeed 函数后,网页的标题内容是否发生了改变
+     */
+    describe('New Feed Selection',function () {
+        var feedSelection,
+            newFeedSelection;
+        beforeEach(function (done) {
+            feedSelection = $('.header-title').html();
+            loadFeed(1,done);
+        });
+        it('保证当用 loadFeed 函数加载一个新源的时候内容会真的改变',function (done) {
+            newFeedSelection = $('.header-title').html();
+            expect(newFeedSelection).not.toBe(feedSelection);
+            done();
+        });
+    })
 }());
